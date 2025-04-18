99a6b974847e36f11736d7a51044bfb5cd9efdfd
Augusto camati <auustocamati21@gmail.com>
2025-04-19 00:25:23 +0100

    feat: adicionar middleware isAdmin para controle de acesso

---
+++ src/middlewares/isAdmin.js
@@ -0,0 +1,8 @@
+export default function isAdmin(req, res, next) {
+  if (req.user.role !== "ADMIN") {
+    return res
+      .status(403)
+      .json({ error: "Acesso negado. Requer privilégios de administrador." })
+  }
+  next()
+}
