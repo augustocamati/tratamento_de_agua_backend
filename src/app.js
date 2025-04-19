dd029aff9ed21b599a9daf9d1fca5423d4094799
Augusto camati <auustocamati21@gmail.com>
2025-04-19 01:35:44 +0100

    feat: adicionar rotas para gerenciamento de alertas e tratamento de erros

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import helmet from "helmet"
 
 import authRoutes from "./routes/authRoutes.js"
 import sensorRoutes from "./routes/sensorRoutes.js"
+import alertRoutes from "./routes/alertRoutes.js"
 
 const app = express()
 
@@ -12,6 +13,13 @@ app.use(cors())
 app.use(json())
 
 // Rotas
+// router.use("/auth", authRoutes)
+// router.use("/sensors", sensorRoutes)
+// router.use("/stations", stationRoutes)
+// router.use("/cards", cardRoutes)
+// router.use("/reports", reportRoutes)
+
+app.use("/api/alerts", alertRoutes)
 app.use("/api/auth", authRoutes)
 app.use("/api/sensors", sensorRoutes)
 
