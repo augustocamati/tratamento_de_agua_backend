7cf3a8b370dad1ff0cbc105103133e5b60408648
Augusto camati <auustocamati21@gmail.com>
2025-04-20 22:35:36 +0100

    feat: refatorar checkSensorLimits para usar constantes de status

--- src/utils/checkSensorLimits.js
+++ src/utils/checkSensorLimits.js
@@ -1,15 +1,21 @@
+export const SensorStatus = {
+  NORMAL: "NORMAL",
+  WARNING: "WARNING",
+  CRITICAL: "CRITICAL",
+};
+
 export default function checkSensorLimits(data) {
-  let turbidityStatus = "NORMAL"
-  if (data.turbidity > 5.0) turbidityStatus = "CRITICAL"
-  else if (data.turbidity > 3.0) turbidityStatus = "WARNING"
+  let turbidityStatus = SensorStatus.NORMAL;
+  if (data.turbidity > 5.0) turbidityStatus = SensorStatus.CRITICAL;
+  else if (data.turbidity > 3.0) turbidityStatus = SensorStatus.WARNING;
 
-  let tdsStatus = "NORMAL"
-  if (data.tds > 500) tdsStatus = "CRITICAL"
-  else if (data.tds < 50) tdsStatus = "WARNING"
+  let tdsStatus = SensorStatus.NORMAL;
+  if (data.tds > 500) tdsStatus = SensorStatus.CRITICAL;
+  else if (data.tds < 50) tdsStatus = SensorStatus.WARNING;
 
-  let flowRateStatus = "NORMAL"
-  if (data.flowRate > 18) flowRateStatus = "CRITICAL"
-  else if (data.flowRate > 15) flowRateStatus = "WARNING"
+  let flowRateStatus = SensorStatus.NORMAL;
+  if (data.flowRate > 18) flowRateStatus = SensorStatus.CRITICAL;
+  else if (data.flowRate > 15) flowRateStatus = SensorStatus.WARNING;
 
-  return { turbidityStatus, tdsStatus, flowRateStatus }
+  return { turbidityStatus, tdsStatus, flowRateStatus };
 }
